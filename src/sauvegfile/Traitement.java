0f348cd95439cd0711390ea0cd5577dde156346d
TheMaster57 <maxime.blaise12@gmail.com>
2016-01-30 10:24:16 +0100

    Début traitement

--- src/sauvegfile/Traitement.java
+++ src/sauvegfile/Traitement.java
@@ -7,5 +7,43 @@ package sauvegfile;
  */
 public class Traitement {
 
-    
+    /**
+     * Chemin vers le fichier de résultat .
+     */
+    public static String cheminVersFichierResultat = "data/resultat.txt";
+
+    /**
+     * Chemin vers le dossier de Google.
+     */
+    private String cheminVersGoogle;
+
+    /**
+     * Chemin vers le dossier de OneDrive.
+     */
+    private String cheminVersOneDrive;
+
+    /**
+     * Memoire temporaire.
+     */
+    private String memoire="";
+
+    /**
+     * Créer un traitement.
+     *
+     * @param cheminVersGoogle Chemin
+     * @param cheminVersOneDrive Chemin
+     */
+    public Traitement(String cheminVersGoogle, String cheminVersOneDrive) {
+        this.cheminVersGoogle = cheminVersGoogle;
+        this.cheminVersOneDrive = cheminVersOneDrive;
+    }
+
+
+    public void traiter() {
+        traiter(this.cheminVersGoogle, this.cheminVersOneDrive, this.memoire);
+    }
+
+    public void traiter(String google, String onedrive, String memoire) {
+
+    }
 }
